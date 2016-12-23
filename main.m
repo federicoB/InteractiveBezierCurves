7e94ad479cd6c008c2f1c86b9693c71424900688
federicoB <federicobertani96@gmail.com>
2016-12-23 11:38:35 +0100

    Added a button for clear the plot

--- main.m
+++ main.m
@@ -3,47 +3,60 @@ clear
 %create a figure with defined title and hide showing figure number in title
 figureHandler=figure('Name','Interactive Bezier Curves','NumberTitle','off');
 %hide the menu
-figureHandler.MenuBar='none';
+figureHandler.MenuBar='none'; 
 %use the same length for the data units along each axis
 axis equal;
 %set axis limits
 axis([0 1 0 1]);
 %set hold state to on so adding new points doesn't delete old points
 hold on
-%define flag for knowing if the user want a closed line
-closed=0;
-%initialize number of points counter
-numberOfPoints=0;
-%matlab doesn't support do-while loop so first we set a true contition
-buttonClicked=1;
-%while the user had clicked the left mouse button
-while (buttonClicked==1)
-    %wait for user click
-    [clickX,clickY,buttonClicked]=ginput(1);
-    %if the left of central mouse button is pressed
-    if (buttonClicked==1) || (buttonClicked==2)
-        %if the central mouse buton is pressed
-        if buttonClicked==2
-            %set the curve as a closed curve
-            closed=1;
-            %set the control point equal to the first control point for closing
-            %the curve
-            clickX=controlPoints(1,1);
-            clickY=controlPoints(2,1);
+%call function to draw a new bezier curve
+drawNewCurve();
+%when a curve is already draw, add the button to clear it and draw a new
+%curve
+btn = uicontrol('Style', 'pushbutton', 'String', 'Clear','Position', [20 20 60 30],'Callback', @(src,event)drawNewCurve);
+
+function drawNewCurve() 
+    %clear current plots
+    cla;
+    %define flag for knowing if the user want a closed line
+    closed=0;
+    %initialize number of points counter
+    numberOfPoints=0;
+    %initialize control points vector
+    controlPoints = [];
+    %matlab doesn't support do-while loop so first we set a true contition
+    buttonClicked=1;
+    %while the user had clicked the left mouse button
+    while (buttonClicked==1)
+        %wait for user click
+        [clickX,clickY,buttonClicked]=ginput(1);
+        %if the left of central mouse button is pressed
+        if (buttonClicked==1) || (buttonClicked==2)
+            %if the central mouse buton is pressed
+            if buttonClicked==2
+                %set the curve as a closed curve
+                closed=1;
+                %set the control point equal to the first control point for closing
+                %the curve
+                clickX=controlPoints(1,1);
+                clickY=controlPoints(2,1);
+            end
+            %increase the number of points counter
+            numberOfPoints=numberOfPoints+1;
+            %set in the qx/qy array the coordinate
+            controlPoints(1,numberOfPoints)=clickX;
+            controlPoints(2,numberOfPoints)=clickY;
+            %plot a blue cicle at the given coordinates
+            plot(controlPoints(1,numberOfPoints),controlPoints(2,numberOfPoints),'bo');
         end
-        %increase the number of points counter
-        numberOfPoints=numberOfPoints+1;
-        %set in the qx/qy array the coordinate
-        controlPoints(1,numberOfPoints)=clickX;
-        controlPoints(2,numberOfPoints)=clickY;
-        %plot a blue cicle at the given coordinates
-        plot(controlPoints(1,numberOfPoints),controlPoints(2,numberOfPoints),'bo');
+    end
+    if (~isempty(controlPoints)) 
+        % draw control polygonal
+        plot(controlPoints(1,:),controlPoints(2,:),'g-');
+        %calculate bezier curve
+        bezierCurve = calculateBezier(controlPoints,numberOfPoints);
+        %draw red solid line bézier curve
+        plot(bezierCurve(1,:),bezierCurve(2,:),'r-'); 
     end
 end
-
-% draw control polygonal
-plot(controlPoints(1,:),controlPoints(2,:),'g-');
-%calculate bezier curve
-bezierCurve = calculateBezier(controlPoints,numberOfPoints);
-%draw red solid line bézier curve
-plot(bezierCurve(1,:),bezierCurve(2,:),'r-');
